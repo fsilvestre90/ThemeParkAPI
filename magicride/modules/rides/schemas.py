29c55776fcda13f5249800975cacffded9220991
Filipe Silvestre <fssilvestre5@gmail.com>
2017-08-06 16:39:51 -0700

    modify schemas for ride and park

--- magicride/modules/rides/schemas.py
+++ magicride/modules/rides/schemas.py
@@ -5,12 +5,13 @@ from utilities import ModelSchema
 class BaseRideSchema(ModelSchema):
     class Meta:
         model = Ride
+        exclude = (
+            Ride.park.key,
+        )
 
 
 class RideNoReviews(BaseRideSchema):
     class Meta:
         exclude = (
-            Ride.id.key,
-            Ride.park.key,
             Ride.reviews.key,
         )
