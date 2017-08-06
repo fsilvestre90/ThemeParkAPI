2f92117367554275129c14c57d00ffa2b486e437
Filipe Silvestre <fssilvestre5@gmail.com>
2017-08-06 15:52:59 -0700

    add delete park by id endpoint

--- magicride/modules/parks/resources.py
+++ magicride/modules/parks/resources.py
@@ -1,11 +1,13 @@
 import logging
 
+from magicride.extensions import db
 from magicride.extensions.api import api_v1
 from magicride.modules.geo import parameters
 from magicride.modules.geo.models import Location
 from magicride.modules.parks import schemas
 from magicride.modules.parks.models import Park
 from utilities import Resource
+from utilities._http import HTTPStatus
 
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -26,13 +28,25 @@ class AllParks(Resource):
 @parks_ns.route('/<int:park_id>')
 @parks_ns.resolve_object_by_model(Park, 'park')
 class ParkByID(Resource):
-
     @parks_ns.response(schemas.BaseParkSchema())
     def get(self, park):
         """
         Get park details by ID.
         """
         return park
+
+    @parks_ns.response(code=HTTPStatus.CONFLICT)
+    @parks_ns.response(code=HTTPStatus.NO_CONTENT)
+    def delete(self, team):
+        """
+        Delete a park by ID.
+        """
+        with parks_ns.commit_or_abort(
+                db.session,
+                default_error_message="Failed to delete the park."
+        ):
+            parks_ns.session.delete(team)
+        return None
 
 
 @parks_ns.route('/nearest')
